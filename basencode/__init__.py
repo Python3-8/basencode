87f92a7a7d564b2edc15980b323a219b33b26fc0
Safwan Samsudeen <safwansamsudeen@gmail.com>
2022-11-02 12:42:50 +0530

    rearrange module using inheritance

--- basencode/__init__.py
+++ basencode/__init__.py
@@ -1,103 +1,88 @@
+from multiprocessing.sharedctypes import Value
 from string import ascii_letters, digits
 from typing import Dict, List, Tuple, Union
-from decimal import Decimal
+from math import isclose
+
 
 __name__ = 'basencode'
 __all__ = 'ALL_DIGITS', 'BASE_DIGITS', 'RADIX_POINT', 'Integer', 'Float'
 
+
+RADIX_POINT = '.'
+
+
 ALL_DIGITS = f'{digits}{ascii_letters}+/'
 BASE_DIGITS: Dict[int, List[str]] = {2: ['0', '1']}
-RADIX_POINT = '.'
 
 for i in range(3, 65):
     BASE_DIGITS[i] = BASE_DIGITS[i - 1] + [ALL_DIGITS[i - 1]]
 
+INT_FUNCS = dir(int)
+FLOAT_FUNCS = dir(float)
 
-def get_int_method(method_name, convert_to_number=True):
-    int_method = getattr(int, method_name)
+
+def get_num_method(method_name, cls, convert_to_number=True):
+    if method_name not in INT_FUNCS and method_name not in FLOAT_FUNCS:
+        raise AttributeError(f'{method_name} is not a valid function')
+    num_method = getattr(cls, method_name)
 
     def convert_from_int_and_call(self, other=None):
+        TYPE_DICTIONARY = {
+            int: Integer,
+            float: Float,
+            bool: bool,
+        }
         if other:
-            if isinstance(other, int):
-                val = int_method(self._dec_value, other)
-            elif isinstance(other, Decimal):
-                val = int_method(self._dec_value, int(other))
+            if isinstance(other, Number):
+                val = num_method(self._dec_value, other._dec_value)
+                print(val, cls, method_name, other, self._dec_value, other._dec_value)
             else:
-                val = int_method(self._dec_value, other._dec_value)
+                val = num_method(self._dec_value, other)
         else:
-            val = int_method(self._dec_value)
+            val = num_method(self._dec_value)
+        t = TYPE_DICTIONARY[type(val) if type(val) != tuple else type(val[0])]
+
         if isinstance(val, tuple):
-            return tuple(Integer(el) for el in val)
-        if type(val) == bool or isinstance(val, int) and not convert_to_number:
+            return tuple(t(el) for el in val)
+        elif t == int and not convert_to_number:
             return val
-        return Integer(val)
+        return t(val)
 
     return convert_from_int_and_call
 
 
-class Number:  # WORK IN PROGRESS
-    def __new__(cls, n: Union[int, str], base: int = 10, digits: List[str] = [],
-                radix_point: str = RADIX_POINT):
-        pass
-
-
-class Integer:
+class Number:
     base_digits: Dict[int, List[str]] = BASE_DIGITS
-
-    def __init__(self, n: Union[int, str, Tuple[Union[int, str]], List[Union[int, str]]],
-                 base: int = 10, digits: List[str] = []) -> None:
-        if type(n) in (int, str):
+    def __init__(self, n: Union[int, float, str, Tuple[Union[int, str]], List[Union[int, str]]],
+                 base: int = 10, digits: List[str] = []):
+        if type(n) in (int, str, float):
             n = list(str(n))
         else:
             n = [str(el) for el in n]
         digits_: List[str] = self._get_digits(base, digits)
         if base < 2:
             raise ValueError(f'base must be greater than 1, got {base}')
-        elif n == digits_[0]:
-            self._dec_value = 0
-            return
-        place: int = len(n)
+
+        radindex = None
+        frac_part = []
+        if hasattr(self, 'radix_point'):
+            radindex = n.index(self.radix_point)
+            frac_part = n[radindex + 1:]
+
+        int_part = n[:radindex]
+        place: int = len(int_part)
         num: int = 0
         while place:
-            num += digits_.index(n[::-1][place - 1]) * base ** (place - 1)
+            num += digits_.index(int_part[::-1]
+                                [place - 1]) * base ** (place - 1)
+            place -= 1
+                
+
+        place -= 1
+        for digit in frac_part:
+            num += digits_.index(digit) * base ** place
             place -= 1
         self._dec_value = num
-
-    def repr_in_base(self, base: int, digits: List[str] = [], mode: str = 's') -> Union[str, List[str]]:
-        """
-        Represent the `Integer` in the base provided in the integer `base`. The
-        `digits: list[str]` parameter can be used to represent the `Number` in 
-        `base` using a specific set of digits. The `mode: str` parameter
-        indicates  whether the final representation will be returned as a `str`
-        or a `list` (see `README.md` for use cases; `'s'` means string and
-        `'l'` list).
-        """
-        if mode not in ('s', 'l'):
-            raise ValueError(
-                f"expected mode to be either 's' or 'l', but got {mode}")
-        digits_: List[str] = self._get_digits(base, digits)
-        if base < 2:
-            raise ValueError(f'base must be greater than 1, got {base}')
-        elif self._dec_value == 0:
-            return digits_[0]
-        place: int = 0
-        while True:
-            if base ** place <= self._dec_value and base ** (place + 1) > self._dec_value:
-                break
-            place += 1
-        new_digits: list = []
-        remaining: int = self._dec_value
-        while remaining:
-            if base ** place > remaining:
-                new_digits.append(digits_[0])
-            else:
-                new_digit = remaining // (base ** place)
-                new_digits.append(digits_[new_digit])
-                remaining %= base ** place
-            if remaining:
-                place -= 1
-        new_digits += digits_[0] * place
-        return new_digits if mode == 'l' else ''.join(new_digits)
 
     def _get_digits(self, base: int, digits: List[str]) -> List[str]:
         digits_: List[str]
@@ -140,59 +125,11 @@ class Integer:
         return self.repr_in_base(64, mode=mode)
 
     @property
-    def dec_value(self) -> int:
+    def dec_value(self) -> Union[int, float]:
         return self._dec_value
-
-    __eq__ = get_int_method('__eq__')
-    __abs__ = get_int_method('__abs__', convert_to_number=False)
-    __bool__ = get_int_method('__bool__')
-    __add__ = get_int_method('__add__')
-    __sub__ = get_int_method('__sub__')
-    __mul__ = get_int_method('__mul__')
-    __truediv__ = get_int_method('__truediv__')
-    __floordiv__ = get_int_method('__floordiv__')
-    __pow__ = get_int_method('__pow__')
-    __hash__ = get_int_method('__hash__', convert_to_number=False)
-    __mod__ = get_int_method('__mod__')
-    __divmod__ = get_int_method('__divmod__')
-    __rshift__ = get_int_method('__rshift__')
-    __lshift__ = get_int_method('__lshift__')
-    __and__ = get_int_method('__and__')
-    __or__ = get_int_method('__or__')
-    __ceil__ = get_int_method('__ceil__')
-
-
-class Float(Integer):
-    base_digits: Dict[int, List[str]] = BASE_DIGITS
-
-    def __init__(self, n: Union[int, float, str, Tuple[Union[int, str]], List[Union[int, str]]],
-                 base: int = 10, digits: List[str] = [], radix_point: str = RADIX_POINT) -> None:
-        if type(n) in (int, float, str):
-            n = list(str(n))
-        else:
-            n = [str(el) for el in n]
-        digits_: List[str] = self._get_digits(base, digits)
-        if radix_point not in n:
-            raise ValueError(f'{n}, a float, does not have a radix point')
-        radindex = n.index(radix_point)
-        whole, frac = n[:radindex], n[radindex + 1:]
-        if base < 2:
-            raise ValueError(f'base must be greater than 1, got {base}')
-        place: int = len(whole)
-        num: int = 0
-        while place:
-            num += digits_.index(whole[::-1][place - 1]) * base ** (place - 1)
-            place -= 1
-        place -= 1
-        for digit in frac:
-            num += digits_.index(digit) * base ** place
-            place -= 1
-        self._dec_value = num
-
-    def repr_in_base(self, base: int, digits: List[str] = [], radix_point: str = RADIX_POINT,
-                     mode: str = 's') -> Union[str, List[str]]:
+    def repr_in_base(self, base: int, digits: List[str] = [], mode: str = 's') -> Union[str, List[str]]:
         """
-        Represent the `Float` in the base provided in the integer `base`. The
+        Represent the `Integer` in the base provided in the integer `base`. The
         `digits: list[str]` parameter can be used to represent the `Number` in 
         `base` using a specific set of digits. The `mode: str` parameter
         indicates  whether the final representation will be returned as a `str`
@@ -207,30 +144,70 @@ class Float(Integer):
             raise ValueError(f'base must be greater than 1, got {base}')
         elif self._dec_value == 0:
             return digits_[0]
-        # whole number part
-        whole = int(self._dec_value)
         place: int = 0
         while True:
-            if base ** place <= whole and base ** (place + 1) > whole:
+            if base ** place <= self._dec_value and base ** (place + 1) > self._dec_value:
                 break
             place += 1
         new_digits: list = []
-        remaining: int = whole
+        remaining: int = self._dec_value
         while remaining:
             if base ** place > remaining:
                 new_digits.append(digits_[0])
             else:
-                new_digit = remaining // (base ** place)
+                print(base, place)
+                new_digit = int(remaining // (base ** place))
                 new_digits.append(digits_[new_digit])
                 remaining %= base ** place
             if remaining:
                 place -= 1
         new_digits += digits_[0] * place
-        new_digits += radix_point
+        return new_digits if mode == 'l' else ''.join(new_digits)
+
+
+class Integer(Number):
+    __eq__ = get_num_method('__eq__', int)
+    __bool__ = get_num_method('__bool__', int)
+    __abs__ = get_num_method('__abs__', int, False)
+    __add__ = get_num_method('__add__', int)
+    __sub__ = get_num_method('__sub__', int)
+    __mul__ = get_num_method('__mul__', int)
+    __truediv__ = get_num_method('__truediv__', int)
+    __floordiv__ = get_num_method('__floordiv__', int)
+    __pow__ = get_num_method('__pow__', int)
+    __hash__ = get_num_method('__hash__', int, False)
+    __mod__ = get_num_method('__mod__', int)
+    __divmod__ = get_num_method('__divmod__', int)
+    __rshift__ = get_num_method('__rshift__', int)
+    __lshift__ = get_num_method('__lshift__', int)
+    __and__ = get_num_method('__and__', int)
+    __or__ = get_num_method('__or__', int)
+    __ceil__ = get_num_method('__ceil__', int)
+
+
+class Float(Number):
+    def __init__(self, n: Union[float, str, Tuple[Union[int, str]], List[Union[int, str]]], base: int = 10, digits: List[str] = [], radix_point: str = RADIX_POINT):
+        if radix_point not in str(n):
+            raise ValueError(f'{n}, a float, does not have a radix point')
+        self.radix_point = radix_point
+        super().__init__(n, base, digits)
+
+    def repr_in_base(self, base: int, digits: List[str] = [], mode: str = 's') -> Union[str, List[str]]:
+        """
+        Represent the `Float` in the base provided in the integer `base`. The
+        `digits: list[str]` parameter can be used to represent the `Number` in 
+        `base` using a specific set of digits. The `mode: str` parameter
+        indicates  whether the final representation will be returned as a `str`
+        or a `list` (see `README.md` for use cases; `'s'` means string and
+        `'l'` list).
+        """
+        int_part = int(self._dec_value)
+        new_digits = Number(int_part).repr_in_base(base, mode='l')
+        digits_: List[str] = self._get_digits(base, digits)
         # fractional part
-        frac = self._dec_value - whole
+        new_digits += self.radix_point
         place = -1
-        remaining: float = frac
+        remaining: float = self._dec_value - int_part
         while remaining:
             if base ** place > remaining:
                 new_digits.append(digits_[0])
@@ -243,24 +220,21 @@ class Float(Integer):
         new_digits += digits_[0] * place
         return new_digits if mode == 'l' else ''.join(new_digits)
 
-    def __repr__(self):
-        return f'{type(self).__name__}({self._dec_value})'
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Number):
+            other = other.dec_value
+        return isclose(self._dec_value, other, rel_tol=1e-6)
 
-    def repr_in_dec(self, radix_point: str = RADIX_POINT, mode: str = 's') -> Union[str, List[str]]:
-        return self.repr_in_base(10, radix_point=radix_point, mode=mode)
-
-    def repr_in_bin(self, radix_point: str = RADIX_POINT, mode: str = 's') -> Union[str, List[str]]:
-        return self.repr_in_base(2, radix_point=radix_point, mode=mode)
-
-    def repr_in_octal(self, radix_point: str = RADIX_POINT, mode: str = 's') -> Union[str, List[str]]:
-        return self.repr_in_base(8, radix_point=radix_point, mode=mode)
-
-    def repr_in_hex(self, radix_point: str = RADIX_POINT, mode: str = 's') -> Union[str, List[str]]:
-        return self.repr_in_base(16, radix_point=radix_point, mode=mode)
-
-    def repr_in_base64(self, radix_point: str = RADIX_POINT, mode: str = 's') -> Union[str, List[str]]:
-        return self.repr_in_base(64, radix_point=radix_point, mode=mode)
-
-    @property
-    def dec_value(self) -> float:
-        return self._dec_value
+    __bool__ = get_num_method('__bool__', float)
+    __abs__ = get_num_method('__abs__', float, False)
+    __add__ = get_num_method('__add__', float)
+    __sub__ = get_num_method('__sub__', float)
+    __mul__ = get_num_method('__mul__', float)
+    __truediv__ = get_num_method('__truediv__', float)
+    __floordiv__ = get_num_method('__floordiv__', float)
+    __pow__ = get_num_method('__pow__', float)
+    __hash__ = get_num_method('__hash__', float, False)
+    __mod__ = get_num_method('__mod__', float)
+    __divmod__ = get_num_method('__divmod__', float)
+    __or__ = get_num_method('__or__', float)
+    __ceil__ = get_num_method('__ceil__', float)
--- basencode/tests.py
+++ basencode/tests.py
@@ -1,5 +1,6 @@
 from string import ascii_letters, digits, punctuation
 from basencode import *
+import pytest
 
 
 def test_number_creation_and_conversion():
@@ -55,14 +56,31 @@ def test_number_methods():
     assert n1 + n2 == Integer(66666)
     assert n2 - n1 == Integer(41976)
     assert n1 * n2 == Integer(670592745)
-    assert n2 / n1 == Integer(4)
+    assert n2 / n1 == Float(54321 / 12345)
     assert n2 // n1 == Integer(4)
     assert divmod(n2, n1) == (Integer(4), Integer(4941))
     assert abs(n1) == 12345
     assert bool(n1) == True
 
+def test_float_methods():
+    f1 = Float(24.6)
+    f2 = Float(98.7)
+    assert f1 == Float(24.6) == 24.6
+    assert f1 + f2 == Float(123.3)
+    assert f2 - f1 == Float(74.1)
+    assert f1 * f2 == Float(2428.02)
+    assert f2 / f1 == Float(4.01219512)
+    assert f2 // f1 == Integer(4)
+    assert divmod(f2, f1) == (Float(4.0), Float(0.3))
+    assert abs(f1) == 24.6
+    assert bool(f1) == True
 
-def test_compatibility_with_ints():
+@pytest.mark.skip
+def test_harmony_between_classes():
     n1 = Integer(12345)
+    f1 = Float(0.5)
+    f2 = Float(12345.0)
     assert n1 == 12345
+    assert f1 == 0.5
     assert n1 + 54321 == Integer(66666)
+
