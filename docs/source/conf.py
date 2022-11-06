1ac8627da7a4ca715f06b9a4563218d6011ba6f3
Pranav Balaji Pooruli <pranav.pooruli@gmail.com>
2022-11-06 13:51:19 +0530

    hopefully fix api reference again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
+master_doc = 'source/index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
